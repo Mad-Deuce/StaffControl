382d5df129a7601b4c8e9831809f76dc9715fbbe
admin <zolotukhinpv@gmail.com>
2021-01-24 19:39:48 +0200

    mainPage.blade.php contented

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -1,1 +1,15 @@
 @extends('layouts.main')
+
+@section('title', 'Main Page')
+
+@section('sidebar_content')
+    <ul>
+        <li><a href="/worker-list">Workers list</a></li>
+        <li><a href="/schedule">Schedule</a></li>
+        <li><a href="/time-sheet">Time sheet</a></li>
+    </ul>
+@endsection
+
+@section('mainFrame_content')
+    Empty
+@endsection
