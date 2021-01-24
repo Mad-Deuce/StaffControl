20f5427a413934f2b8521d419b2af4c1730b3029
admin <zolotukhinpv@gmail.com>
2021-01-24 22:01:49 +0200

    changed Model "Worker" 2

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -2,11 +2,11 @@
 
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\SoftDeletes;
+//use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Worker extends \Illuminate\Database\Eloquent\Model
 {
-    use SoftDeletes;
+//    use SoftDeletes;
 
-    protected $dates=['deleted_at'];
+ //   protected $dates=['deleted_at'];
 }
