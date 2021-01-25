30137d3b6e416fd7562ea83696c3dc8766d7d290
admin <zolotukhinpv@gmail.com>
2021-01-25 23:43:52 +0200

    Add form "ADD" 12

--- app/Http/Controllers/WorkerController.php
+++ app/Http/Controllers/WorkerController.php
@@ -19,20 +19,20 @@ class WorkerController extends Controller
     }
 
     public function addOne(Request $request){
-        if ($request->has('worker-tab_number')) {
-            $findWorker=Worker::where('tab_number', $request->worker-tab_number)->first();
+        if ($request->has('tab_number')) {
+            $findWorker=Worker::where('tab_number', $request->tab_number)->first();
             if (isset($findWorker)){
                 $request->session()->flash('status', 'Работник с таким табельным номером существует');
                 return view ('Workers.addOne');
             } else {
                 $worker = new Worker;
-                $worker->name=$request->worker-name;
-                $worker->surname=$request->worker-surname;
-                $worker->patronymic=$request->worker-patronymic;
-                $worker->birthday=$request->worker-birthday;
-                $worker->gender=$request->worker-gender;
-                $worker->tab_number=$request->worker-tab_number;
-                $worker->start_working=$request->worker-start_working;
+                $worker->name=$request->name;
+                $worker->surname=$request->surname;
+                $worker->patronymic=$request->patronymic;
+                $worker->birthday=$request->birthday;
+                $worker->gender=$request->gender;
+                $worker->tab_number=$request->tab_number;
+                $worker->start_working=$request->start_working;
                 $worker->save();
 
                 $request->session()->flash('status', 'Работник добавлен');
--- resources/views/Workers/addOne.blade.php
+++ resources/views/Workers/addOne.blade.php
@@ -5,17 +5,17 @@
 @section('mainFrame_content')
     <h4>Main Frame</h4>
     <form action="/worker-add">
-        <input type="text" name="worker-name">Имя<Br>
-        <input type="text" name="worker-surname">фамилия<Br>
-        <input type="text" name="worker-patronymic">Отчество<Br>
-        <input type="date" name="worker-birthday">Дата рождения<Br>
+        <input type="text" name="name">Имя<Br>
+        <input type="text" name="surname">фамилия<Br>
+        <input type="text" name="patronymic">Отчество<Br>
+        <input type="date" name="birthday">Дата рождения<Br>
         <fieldset>
             <legend>Пол</legend>
-            <input type="radio" name="worker-gender" value="ч">ч
-            <input type="radio" name="worker-gender" value="ж">ж
+            <input type="radio" name="gender" value="ч">ч
+            <input type="radio" name="gender" value="ж">ж
         </fieldset><Br>
-        <input type="number" name="worker-tab_number">Табельный номер<Br>
-        <input type="date" name="worker-start_working">Дата принятия на работу<Br>
+        <input type="number" name="tab_number">Табельный номер<Br>
+        <input type="date" name="start_working">Дата принятия на работу<Br>
         <input type="submit" value="Добавить"><Br>
     </form>
 @endsection
