59241a380a3d6af1802ecee859cab19bd5120a2c
admin <zolotukhinpv@gmail.com>
2021-01-30 18:15:22 +0200

    add график

---
+++ resources/views/Schedule/showAll.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.main')
+
+@section('title', 'Schedule')
+
+@section('mainFrame_content')
+    <h4>Main Frame</h4>
+     <table>
+        <tr>
+            <th>№ з/п</th>
+            <th>Прізвище, ініціали</th>
+            <th>Посада</th>
+            @for ($i = 1; $i < 32; $i++)
+                <th>{{ $i }}</th>
+            @endfor
+            <th>Години</th>
+            <th>Підпис</th>
+        </tr>
+    </table>
+    <td><a href="/worker-add">ADD</a></td><BR>
+    <td><a href="/worker-import">IMPORT FROM *.xls</a></td><BR>
+@endsection
