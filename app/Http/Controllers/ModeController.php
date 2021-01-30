149ac8a9f42be4a0e28bb6bacf4760e935688135
admin <zolotukhinpv@gmail.com>
2021-01-30 23:48:34 +0200

    add график

--- app/Http/Controllers/ModeController.php
+++ app/Http/Controllers/ModeController.php
@@ -13,8 +13,9 @@ class ModeController extends Controller
     //
     public function addOne($worker_id, Request $request){
         $worker = Worker::where('id', $worker_id)->first();
-        $mode_codes = Mode_code::all();
-
+        //$mode_codes = Mode_code::all();
+        $mode_codes_non_app = Mode_code::where('category','non-appearance');
+        $mode_codes_app = Mode_code::where('category','appearance');
         if ($request->has('mode_add')) {
             $mode = new Mode;
             $mode->worker_id = $worker_id;
@@ -25,7 +26,7 @@ class ModeController extends Controller
             return redirect('/worker-list');
 
         } else {
-            return view('Modes.addOne', ['worker'=>$worker, 'mode_codes'=>$mode_codes]);
+            return view('Modes.addOne', ['worker'=>$worker, 'mode_codes_non_app'=>$mode_codes_non_app, 'mode_codes_app'=>$mode_codes_app]);
         }
     }
 
