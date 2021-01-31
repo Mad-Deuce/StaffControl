a077392b3df1ed272848f733179e53e796b08fe2
admin <zolotukhinpv@gmail.com>
2021-01-31 20:58:19 +0200

    add func "add_from_modes"

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -31,7 +31,7 @@ class ScheduleController extends Controller
             foreach ($findModes as $findMode) {
                 print_r($findMode);
                 echo ('<BR>');
-                for ($i=$findMode['start_mode']; $i<=$findMode['end_mode'];$i++) {
+                for ($i=$findMode['start_mode']; $i<=$findMode['end_mode'];$i=date_add($i, DateInterval('P1D'))) {
                     $schedule=new Schedule();
                     $schedule->worker_id = $findMode['worker_id'];
                     $schedule->day_of_month = $i;
