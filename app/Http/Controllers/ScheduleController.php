53be87151480b23338f4d7faa3df18a892182697
admin <zolotukhinpv@gmail.com>
2021-01-31 23:26:57 +0200

    add func "add_from_modes"3

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -23,8 +23,10 @@ class ScheduleController extends Controller
     }
 
     public function add_from_modes(){
-        $findModes = Mode:: where('start_mode','>=', '2021-01-01')->
-                            where('end_mode','<=', '2021-01-31')->get();
+        $firstDayOfMonth='2021-01-01';
+        $lastDayOfMonth='2021-01-31';
+        $findModes = Mode:: where('start_mode','>=', $firstDayOfMonth)->
+                            where('end_mode','<=', $lastDayOfMonth)->get();
         if (isset($findModes)) {
             foreach ($findModes as $findMode) {
                 $i = date_create($findMode->start_mode);
@@ -44,7 +46,75 @@ class ScheduleController extends Controller
                 echo ('OK');
             }
         } else {
-            return ('$findModes is null');
+            $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
+                                where('end_mode','<=', $lastDayOfMonth)->get();
+            if (isset($findModes)) {
+                foreach ($findModes as $findMode) {
+                    $i = date_create($firstDayOfMonth);
+                    $k = date_create($findMode->end_mode);
+                    for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                        $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                        where('day_of_month','=',$i)->
+                        where('mode_code_id','=',$findMode->mode_code_id)->first();
+                        if (isset($findSchedule)==false) {
+                            $schedule = new Schedule();
+                            $schedule->worker_id = $findMode->worker_id;
+                            $schedule->day_of_month = $i;
+                            $schedule->mode_code_id = $findMode->mode_code_id;
+                            $schedule->save();
+                        }
+                    }
+                    echo ('OK');
+                }
+            } else {
+                $findModes = Mode:: where('start_mode','>=', $firstDayOfMonth)->
+                                    where('end_mode','>', $lastDayOfMonth)->get();
+                if (isset($findModes)) {
+                    foreach ($findModes as $findMode) {
+                        $i = date_create($findMode->start_mode);
+                        $k = date_create($lastDayOfMonth);
+                        for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                            $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                                                    where('day_of_month','=',$i)->
+                                                    where('mode_code_id','=',$findMode->mode_code_id)->first();
+                            if (isset($findSchedule)==false) {
+                                $schedule = new Schedule();
+                                $schedule->worker_id = $findMode->worker_id;
+                                $schedule->day_of_month = $i;
+                                $schedule->mode_code_id = $findMode->mode_code_id;
+                                $schedule->save();
+                            }
+                        }
+                        echo ('OK');
+                    }
+                } else {
+                    $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
+                                        where('end_mode','>', $lastDayOfMonth)->get();
+                    if (isset($findModes)) {
+                        foreach ($findModes as $findMode) {
+                            $i = date_create($firstDayOfMonth);
+                            $k = date_create($lastDayOfMonth);
+                            for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                                $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                                                        where('day_of_month','=',$i)->
+                                                        where('mode_code_id','=',$findMode->mode_code_id)->first();
+                                if (isset($findSchedule)==false) {
+                                    $schedule = new Schedule();
+                                    $schedule->worker_id = $findMode->worker_id;
+                                    $schedule->day_of_month = $i;
+                                    $schedule->mode_code_id = $findMode->mode_code_id;
+                                    $schedule->save();
+                                }
+                            }
+                            echo ('OK');
+                        }
+                    }
+
+
+                }
+
+            }
+            echo ('Неявки не найдены');
         }
 
     }
