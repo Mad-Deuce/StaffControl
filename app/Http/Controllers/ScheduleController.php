5b43c3e772a33e61a9efae819f375ed0b81f89f0
admin <zolotukhinpv@gmail.com>
2021-01-31 23:50:05 +0200

    add func "add_from_modes"4

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -25,6 +25,7 @@ class ScheduleController extends Controller
     public function add_from_modes(){
         $firstDayOfMonth=date_create('2021-01-01');
         $lastDayOfMonth=date_create('2021-01-31');
+
         $findModes = Mode:: where('start_mode','>=', $firstDayOfMonth)->
                             where('end_mode','<=', $lastDayOfMonth)->get();
         if (isset($findModes)) {
@@ -45,76 +46,72 @@ class ScheduleController extends Controller
                 }
                 echo ('OK');
             }
-        } else {
-            $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
-                                where('end_mode','<=', $lastDayOfMonth)->get();
-            if (isset($findModes)) {
-                foreach ($findModes as $findMode) {
-                    $i = date_create($firstDayOfMonth);
-                    $k = date_create($findMode->end_mode);
-                    for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
-                        $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
-                        where('day_of_month','=',$i)->
-                        where('mode_code_id','=',$findMode->mode_code_id)->first();
-                        if (isset($findSchedule)==false) {
-                            $schedule = new Schedule();
-                            $schedule->worker_id = $findMode->worker_id;
-                            $schedule->day_of_month = $i;
-                            $schedule->mode_code_id = $findMode->mode_code_id;
-                            $schedule->save();
-                        }
+        }
+
+        $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
+                            where('end_mode','<=', $lastDayOfMonth)->get();
+        if (isset($findModes)) {
+            foreach ($findModes as $findMode) {
+                $i = date_create($firstDayOfMonth);
+                $k = date_create($findMode->end_mode);
+                for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                    $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                                            where('day_of_month','=',$i)->
+                                            where('mode_code_id','=',$findMode->mode_code_id)->first();
+                    if (isset($findSchedule)==false) {
+                        $schedule = new Schedule();
+                        $schedule->worker_id = $findMode->worker_id;
+                        $schedule->day_of_month = $i;
+                        $schedule->mode_code_id = $findMode->mode_code_id;
+                        $schedule->save();
                     }
-                    echo ('OK');
                 }
-            } else {
-                $findModes = Mode:: where('start_mode','>=', $firstDayOfMonth)->
-                                    where('end_mode','>', $lastDayOfMonth)->get();
-                if (isset($findModes)) {
-                    foreach ($findModes as $findMode) {
-                        $i = date_create($findMode->start_mode);
-                        $k = date_create($lastDayOfMonth);
-                        for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
-                            $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
-                                                    where('day_of_month','=',$i)->
-                                                    where('mode_code_id','=',$findMode->mode_code_id)->first();
-                            if (isset($findSchedule)==false) {
-                                $schedule = new Schedule();
-                                $schedule->worker_id = $findMode->worker_id;
-                                $schedule->day_of_month = $i;
-                                $schedule->mode_code_id = $findMode->mode_code_id;
-                                $schedule->save();
-                            }
-                        }
-                        echo ('OK');
-                    }
-                } else {
-                    $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
-                                        where('end_mode','>', $lastDayOfMonth)->get();
-                    if (isset($findModes)) {
-                        foreach ($findModes as $findMode) {
-                            $i = date_create($firstDayOfMonth);
-                            $k = date_create($lastDayOfMonth);
-                            for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
-                                $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
-                                                        where('day_of_month','=',$i)->
-                                                        where('mode_code_id','=',$findMode->mode_code_id)->first();
-                                if (isset($findSchedule)==false) {
-                                    $schedule = new Schedule();
-                                    $schedule->worker_id = $findMode->worker_id;
-                                    $schedule->day_of_month = $i;
-                                    $schedule->mode_code_id = $findMode->mode_code_id;
-                                    $schedule->save();
-                                }
-                            }
-                            echo ('OK');
-                        }
-                    }
-
-
-                }
-
+                echo ('OK');
             }
-            echo ('Неявки не найдены');
+        }
+
+        $findModes = Mode:: where('start_mode','>=', $firstDayOfMonth)->
+                            where('end_mode','>', $lastDayOfMonth)->get();
+        if (isset($findModes)) {
+            foreach ($findModes as $findMode) {
+                $i = date_create($findMode->start_mode);
+                $k = date_create($lastDayOfMonth);
+                for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                    $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                                            where('day_of_month','=',$i)->
+                                            where('mode_code_id','=',$findMode->mode_code_id)->first();
+                    if (isset($findSchedule)==false) {
+                        $schedule = new Schedule();
+                        $schedule->worker_id = $findMode->worker_id;
+                        $schedule->day_of_month = $i;
+                        $schedule->mode_code_id = $findMode->mode_code_id;
+                        $schedule->save();
+                    }
+                }
+                echo ('OK');
+            }
+        }
+
+        $findModes = Mode:: where('start_mode','<', $firstDayOfMonth)->
+                            where('end_mode','>', $lastDayOfMonth)->get();
+        if (isset($findModes)) {
+            foreach ($findModes as $findMode) {
+                $i = date_create($firstDayOfMonth);
+                $k = date_create($lastDayOfMonth);
+                for ($i; $i<=$k; $i=date_add($i, date_interval_create_from_date_string("1 day"))) {
+                    $findSchedule=Schedule::where('worker_id','=',$findMode->worker_id)->
+                    where('day_of_month','=',$i)->
+                    where('mode_code_id','=',$findMode->mode_code_id)->first();
+                    if (isset($findSchedule)==false) {
+                        $schedule = new Schedule();
+                        $schedule->worker_id = $findMode->worker_id;
+                        $schedule->day_of_month = $i;
+                        $schedule->mode_code_id = $findMode->mode_code_id;
+                        $schedule->save();
+                    }
+                }
+                echo ('OK');
+            }
         }
         print_r($findModes);
     }
