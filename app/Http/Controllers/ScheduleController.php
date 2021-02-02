0ceb3bf387269ac4c56d1ef6a2cc1a68659eb902
admin <zolotukhinpv@gmail.com>
2021-02-02 22:31:32 +0200

    add func "add_from_modes"5

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -56,13 +56,13 @@ class ScheduleController extends Controller
                 echo ('<BR>');
 
                 if (date_create($findMode->start_mode) < $firstDayOfMonth){
-                    $i = $firstDayOfMonth; //Блять, пиздец, нихуя не пойму
+                    $i = date_create('2021-01-01');; //Блять, пиздец, нихуя не пойму
                 } else {
                     $i = date_create($findMode->start_mode);
                 }
 
                 if (date_create($findMode->end_mode) > $lastDayOfMonth){
-                    $k = $lastDayOfMonth;  //Блять, пиздец, нихуя не пойму
+                    $k = date_create('2021-01-31');  //Блять, пиздец, нихуя не пойму
                 } else {
                     $k = date_create($findMode->end_mode);
                 }
